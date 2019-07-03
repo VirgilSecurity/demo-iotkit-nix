d71fd31fa0d346ca1644dca38108cef0c2122e81
Dmytro Hrytsenko <dm.hrytsenko@gmail.com>
2019-07-03 15:47:24 +0300

    [tests] Add jenkins support for tests

---
+++ scripts/jenkins/build-rpi-gw-tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+check_error() {
+   RETRIES=$?
+   if [ $RETRIES != 0 ]; then
+        echo "----------------------------------------------------------------------"
+        echo "############# !!! PROCESS ERROR ERRORCODE=[$RETRIES] ##################"
+        echo "----------------------------------------------------------------------"
+     exit $RETRIES
+   else
+        echo "-------------Process OK. ---------------------------------------------"
+   fi
+}
+
+# Build User space
+cd "${SCRIPT_FOLDER}/../../"
+echo "----------------------------------------------------------------------"
+echo "################# BUILDING RPI GW TESTS ##############################"
+echo "----------------------------------------------------------------------"
+echo "------------- Preparing for build ------------------------------------"
+rm -rf cmake-build-debug
+mkdir -p cmake-build-debug
+cd ./cmake-build-debug && cmake -DVIRGIL_IOT_MCU_BUILD=OFF $@ "../"
+check_error
+echo "------------- Build rpi_gw_tests -------------------------------------"
+make rpi_gw_tests -j 1
+check_error
+echo "----------------------------------------------------------------------"
+echo "################# FINISH RPI GW TESTS BUILDING #######################"
+echo "----------------------------------------------------------------------"
