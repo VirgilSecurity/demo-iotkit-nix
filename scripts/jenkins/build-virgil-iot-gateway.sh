8a0967afc0a5922ebc05a820f53111a9601e57ea
Dmytro Hrytsenko <dm.hrytsenko@gmail.com>
2019-08-13 16:25:15 +0300

    [testing] Update build-virgil-iot-gateway.sh script

--- scripts/jenkins/build-virgil-iot-gateway.sh
+++ scripts/jenkins/build-virgil-iot-gateway.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-
 SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT="$SCRIPT_FOLDER/../.."
+BUILD_OUTPUT="$PROJECT_ROOT/build-test"
+
+#Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 
 check_error() {
    RETRIES=$?
@@ -15,25 +22,54 @@ check_error() {
    fi
 }
 
-# Build User space
-cd "${SCRIPT_FOLDER}/../../"
+cleanup() {
+   cd "$PROJECT_ROOT"
+   rm -rf cmake-build-debug && mkdir -p cmake-build-debug
+   cd ./cmake-build-debug
+}
+
+rm -rf BUILD_OUTPUT
+mkdir -p $BUILD_OUTPUT
+echo "Files will be stored at $BUILD_OUTPUT"
+
+
 echo "----------------------------------------------------------------------"
 echo "################# BUILDING VIRGIL IOT GATEWAY ########################"
 echo "----------------------------------------------------------------------"
-echo "------------- Preparing for build ------------------------------------"
-rm -rf cmake-build-debug
-mkdir -p cmake-build-debug
-cd ./cmake-build-debug && cmake -DVIRGIL_IOT_MCU_BUILD=OFF $@ "../"
-check_error
 
 echo "------------- Build virgil-iot-gateway-initializer -------------------"
+cleanup
+cmake -DVIRGIL_IOT_MCU_BUILD=OFF $@ "../"
+check_error
 make virgil-iot-gateway-initializer -j 8
 check_error
+mv initializer/virgil-iot-gateway-initializer $BUILD_OUTPUT/
+check_error
 
-echo "------------- Build virgil-iot-gateway-app ---------------------------"
+echo "------------- Build virgil-iot-gateway-app (initial) -----------------"
+cleanup
+cmake -DVIRGIL_IOT_MCU_BUILD=OFF \
+      -DGATEWAY_SIMULATOR=ON \
+      -DTEST_UPDATE_MESSAGE="${RED}[ Hello from initial Gateway firmware ]${NC}" $@ "../"
+check_error
 make virgil-iot-gateway-app -j 8
+check_error
+mv app/virgil-iot-gateway-app $BUILD_OUTPUT/virgil-iot-gateway-app-initial
+check_error
+
+echo "------------- Build virgil-iot-gateway-app (update) -----------------"
+cleanup
+cmake -DVIRGIL_IOT_MCU_BUILD=OFF \
+      -DGATEWAY_SIMULATOR=ON \
+      -DTEST_UPDATE_MESSAGE="${GREEN}[ Hello from updated Gateway firmware ]${NC}" $@ "../"
+check_error
+make virgil-iot-gateway-app -j 8
+check_error
+mv app/virgil-iot-gateway-app $BUILD_OUTPUT/virgil-iot-gateway-app-update
 check_error
 
 echo "----------------------------------------------------------------------"
 echo "################### FINISH VIRGIL IOT GATEWAY  #######################"
+echo "Files are stored to: $BUILD_OUTPUT"
+ls -l $BUILD_OUTPUT
 echo "----------------------------------------------------------------------"
