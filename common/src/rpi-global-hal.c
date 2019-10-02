aa700daa3f33e36c23d5a09cf57de44044456438
Roman Kutashenko <kutashenko@gmail.com>
2019-10-02 06:58:01 +0300

    SDMP:INFO extension (part 1)

--- common/src/rpi-global-hal.c
+++ common/src/rpi-global-hal.c
@@ -43,7 +43,7 @@
 #include <pthread.h>
 
 #include <virgil/iot/protocols/sdmp.h>
-#include <virgil/iot/protocols/sdmp/info.h>
+#include <virgil/iot/protocols/sdmp/info-server.h>
 #include <virgil/iot/logger/logger.h>
 #include <virgil/iot/macros/macros.h>
 #include <virgil/iot/trust_list/trust_list.h>
@@ -56,7 +56,6 @@
 #include "hal/storage/rpi-storage-hal.h"
 
 #include "hal/netif/netif-queue.h"
-#include "hal/netif/rpi-plc-sim.h"
 #include "hal/netif/rpi-udp-broadcast.h"
 #include "hal/storage/rpi-file-io.h"
 
@@ -237,6 +236,8 @@ vs_rpi_start(const char *devices_dir,
              vs_storage_op_ctx_t *fw_ctx,
              const vs_fw_manufacture_id_t manufacture_id,
              const vs_fw_device_type_t device_type) {
+    vs_netif_t *netif = NULL;
+    vs_netif_t *queued_netif = NULL;
 
     vs_logger_init(VS_LOGLEV_DEBUG);
 
@@ -260,22 +261,11 @@ vs_rpi_start(const char *devices_dir,
     fw_ctx->file_sz_limit = VS_MAX_FIRMWARE_UPDATE_SIZE;
     CHECK_RET(!vs_firmware_init(fw_ctx), -2, "Unable to initialize Firmware library");
 
-    if (plc_sim_addr.s_addr == htonl(INADDR_ANY)) {
-        // Setup UDP Broadcast as network interface
-        vs_hal_netif_udp_bcast_force_mac(forced_mac_addr);
+    // Setup UDP Broadcast as network interface
+    vs_hal_netif_udp_bcast_force_mac(forced_mac_addr);
 
-        // Get PLC Network interface
-        netif = vs_hal_netif_udp_bcast();
-    } else {
-        // Setup PLC simulator as network interface
-        vs_hal_netif_plc_force_mac(forced_mac_addr);
-
-        // Set IP of PLC simulator
-        vs_plc_sim_set_ip(plc_sim_addr);
-
-        // Get PLC Network interface
-        netif = vs_hal_netif_plc();
-    }
+    // Get PLC Network interface
+    netif = vs_hal_netif_udp_bcast();
 
     // Prepare queued network interface
     queued_netif = vs_netif_queued(netif);
@@ -284,7 +274,7 @@ vs_rpi_start(const char *devices_dir,
     CHECK_RET(!vs_sdmp_init(queued_netif), -1, "Unable to initialize SDMP");
 
     // Register SDMP:INFO service
-    CHECK_RET(!vs_sdmp_register_service(vs_sdmp_info(tl_ctx, fw_ctx, manufacture_id, device_type)),
+    CHECK_RET(!vs_sdmp_register_service(vs_sdmp_info_server(tl_ctx, fw_ctx, manufacture_id, device_type)),
               -1,
               "INFO service is not registered");
 
